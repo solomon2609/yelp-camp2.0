046585950f9e5de1a57bab125dcacbaa8ed85992
solomon2609 <96571093+solomon2609@users.noreply.github.com>
2024-08-23 16:59:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -138,6 +138,7 @@ app.use((err, req, res, next)=> {
     if(!err.message) err.message = 'Oh no! something went wrong';
     res.status(statusCode).render('errorTemplate',{err});
 })
-app.listen(8080, ()=>{
-    console.log('listining port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
